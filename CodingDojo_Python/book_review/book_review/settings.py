3c1f3fccc68cb5b0668e96698b4ca807a23f6c0d
Anish Gheewala <agheewala1231@gmail.com>
2017-09-24 23:52:33 -0500

    changing files.

--- CodingDojo_Python/book_review/book_review/settings.py
+++ CodingDojo_Python/book_review/book_review/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'di7xw_ubc-@tvhzqj1n96096%($lkfi-lr$ursvnz3q0%5n4cx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
