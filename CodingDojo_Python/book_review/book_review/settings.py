94d427f2fa11c58308a71e4ebc8fab70440f959b
Anish Gheewala <agheewala1231@gmail.com>
2017-09-24 22:59:46 -0500

    Added a Procfile.

--- CodingDojo_Python/book_review/book_review/settings.py
+++ CodingDojo_Python/book_review/book_review/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'di7xw_ubc-@tvhzqj1n96096%($lkfi-lr$ursvnz3q0%5n4cx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'book_review.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
 
 
