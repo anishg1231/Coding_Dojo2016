0b55abeab2acf3d2cff8c8ebf8bd0297f0dcebe7
Anish Gheewala <agheewala1231@gmail.com>
2017-09-26 14:59:32 -0500

    commit

--- CodingDojo_Python/book_review/book_review/production.py
+++ CodingDojo_Python/book_review/book_review/production.py
@@ -1,10 +1,10 @@
-# settings/production.py
-from base import
-import dj_database_url
-
-ENVIRONMENT = 'production'
-DEBUG = False
-ALLOWED_HOSTS = ['0.0.0.0']
-DATABASES['default'] = dj_database_url.config(
-    default='postgres://lbwyslhxfizzds:d9fddb338bb0a177a96ccb7258289f993004299b5b30d2507d332988792ed9ff@ec2-23-21-197-175.compute-1.amazonaws.com:5432/dfnbd703q2t7vn'
-)
+# # settings/production.py
+# from base import
+# import dj_database_url
+#
+# ENVIRONMENT = 'production'
+# DEBUG = False
+# ALLOWED_HOSTS = ['https://django-book-review.herokuapp.com/']
+# DATABASES['default'] = dj_database_url.config(
+#     default='postgres://lbwyslhxfizzds:d9fddb338bb0a177a96ccb7258289f993004299b5b30d2507d332988792ed9ff@ec2-23-21-197-175.compute-1.amazonaws.com:5432/dfnbd703q2t7vn'
+# )
---
+++ CodingDojo_Python/myEvironments/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anishgheewala/Coding_Dojo2016/CodingDojo_Python/myEvironments/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
